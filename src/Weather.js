913602eef678573b8eeab0ab23e8e8cfbca5d5e3
tonaadefokun <84527335+tonaadefokun@users.noreply.github.com>
2021-09-10 13:46:24 +0100

    updated

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Weather() {
   return (
     <div>
-      <button className="btn btn-primary">Click Me</button>
+      <button className="btn btn-primary">Click Me!</button>
     </div>
   );
 }
